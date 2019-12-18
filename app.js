24420c2e7ea842916eeed4951b84dd7f0f60236c
jsantyvizcaino <jorge_santiagovizcaino@hotmail.com>
2019-12-18 11:31:24 -0500

    inicio

---
+++ app.js
@@ -0,0 +1,12 @@
+const axios = require('axios');
+const clima = require('./controlador/clima');
+
+
+clima.getClima(-0.190000, -78.500000).then(console.log);
+
+//-2.210000, -79.900002 guayaquill
+
+//40.419998, -3.700000 madrid
+
+
+//43.720001, 10.400000 pisa italia
